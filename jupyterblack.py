9087acb3e24b488f5286349cca28a731deab4a81
Ira Horecka <ira89@icloud.com>
2020-09-20 14:31:33 -0700

    add requirements.txt and .gitignore -- make changes to jupyterblack.py

--- jupyterblack.py
+++ jupyterblack.py
@@ -1,50 +1,27 @@
 import json
-import safer
+import sys
 import uuid
+import safer
 from black import format_str, FileMode, InvalidInput
 
 
 def open_jupyter(filename):
-    with safer.open(filename, "r") as file:
-        return file.read()
+    with safer.open(filename, "r") as jupyter_infile:
+        return jupyter_infile.read()
 
 
 def parse_jupyter(content):
-    newline_hash = str(uuid.uuid4())
     content_json = json.loads(content)
+    newline_hash = str(uuid.uuid4())
     for cell in content_json["cells"]:
         if cell["cell_type"] == "code":
-            # print(repr("".join(cell["source"])))
-            format_cell_repr = repr(format_black("".join(cell["source"])))
-            ####dev#####
+            blacked_cell_char = [char for char in format_black("".join(cell["source"]))]
+            blacked_cell = "".join(
+                [newline_hash if char == "\n" else char for char in blacked_cell_char]
+            )
+            cell_lines = blacked_cell.split(newline_hash)
+            cell["source"] = [line + "\n" for line in cell_lines]
 
-            black_cell=[i for i in format_black("".join(cell["source"]))]
-            black_cell_newline=''.join([newline_hash if i == "\n" else i for i in black_cell])
-            list_format = black_cell_newline.split(newline_hash)
-            cell["source"] = [i + "\n" for i in list_format]
-            ####dev#####
-            # print(format_cell_repr.split('\\n'))
-            # print(format_cell_repr.encode().decode('unicode_escape'), '\n')
-            # temp_escape_list = format_cell_repr[1:-1].split("\\\\")
-            # escape_exceptions = ("n", "t")
-            # temp_escape_list = [
-            #     "\\" + i if len(i) != 0 and i.startswith(escape_exceptions) else i for i in temp_escape_list
-            # ]
-            # temp_escape_list=["\\\\" if len(i) == 0 else i for i in temp_escape_list]
-            # # print(temp_escape_list)
-            # # replace empty cells with \\\\ to ""
-            # if len(temp_escape_list) == 1 and temp_escape_list[0] == "\\\\":
-            #     temp_escape_list = ['']
-            # temp_escape_str = ''.join(temp_escape_list)
-            # # print(temp_escape_str)
-            # list_format = temp_escape_str.split("\\n")
-            # # print(format_cell_repr)
-            # # print(temp_escape_str)
-            # # print(format_cell_repr)
-            # # list_format = format_cell_repr[1:-1].split("\\n")
-            # # print(temp_escape_list)
-            # # print(list_format)
-            # cell["source"] = [i + "\n" for i in list_format]
     return content_json
 
 
@@ -57,8 +34,8 @@ def format_black(cell_content, line_length=88):
 
 
 def export_jupyter(formatted_content, filename):
-    with open(filename, "w") as file:
-        file.write(json.dumps(formatted_content))
+    with open(filename, "w") as jupyter_outfile:
+        jupyter_outfile.write(json.dumps(formatted_content))
 
 
 if __name__ == "__main__":
